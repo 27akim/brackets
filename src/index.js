434d0d15d3792997feed01af65ba94b7f410a389
27akim <criminal.ok1@gmail.com>
2021-03-04 11:32:54 +0300

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   let brackets = bracketsConfig.flatMap(i => i.join(''));
+  let isString = true;
+  if(parseInt(str) == NaN)
+  {
+    isString = false;
+  }
   for(let i = 0; i < brackets.length; i++)
     {
-      if(str.includes(brackets[i]))
+      if(isString)
+      {
+        if(str.includes(brackets[i]))
       {
         str = str.replace(brackets[i],'');
-        i -= 2;
+        i -= 3;
+      }
+      }
+      else
+      {
+        if(str.includes(parseInt(brackets[i])))
+      {
+        str = str.replace(brackets[i],'');
+        i -= 3;
+      }
       }
     }
   return str.length == 0 ? true : false;
